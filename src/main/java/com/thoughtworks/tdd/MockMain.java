337aa7724f8f86be5e88d109c0d05aa5442b6885
humplee <littleted1221@163.com>
2019-06-18 22:23:03 +0800

    fizzbuzz ini

--- src/main/java/com/thoughtworks/tdd/MockMain.java
+++ src/main/java/com/thoughtworks/tdd/MockMain.java
@@ -1,5 +1,16 @@
 package com.thoughtworks.tdd;
 
 public class MockMain {
-    
+    MockMain(){
+
+    }
+    public String fizzBuzz(int count){
+        String output="";
+        Boolean countReturn = true;
+        if(count>0&&count%3==0){output+="Fizz"; countReturn=false;}
+        if(count>0&&count%5==0){output+="Buzz";countReturn=false;}
+        if(count>0&&count%7==0){output+="Whizz";countReturn=false;}
+        if(!countReturn)return output;
+        return String.valueOf(count);
+    }
 }
